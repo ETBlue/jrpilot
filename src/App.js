4445b31e8a0c062c3ccd0420a75dd20c3b988fea
ETBlue <etblue.tw@gmail.com>
2017-08-18 17:18:49 +0800

    performance: manage static data before rendering

--- src/App.js
+++ src/App.js
@@ -25,10 +25,159 @@ class App extends Component {
 
     this.state = {
       teamfilter: 'all',
-      statusfilter: 'all'
+      statusfilter: 'all',
+      pagefilter: '1'
     }
 
     this._filter = this._filter.bind(this)
+
+  }
+
+  componentWillMount () {
+
+    this._staticData = {}
+
+    this._staticData.about = [
+      {
+        url: 'https://etblue.github.io/jrpilot/',
+        icon: 'globe',
+        text: '範例'
+      },
+      {
+        url: 'https://github.com/ETBlue/jrpilot',
+        icon: 'github',
+        text: '程式碼'
+      },
+      {
+        url: 'https://docs.google.com/spreadsheets/d/1CTGZfJ55WJQEq6UexllrXZJvceVP3TRjzNGMgxE-I68/edit?usp=sharing',
+        icon: 'table',
+        text: '資料'
+      },
+      {
+        url: 'https://docs.google.com/presentation/d/18QjhH784zyxAxGue7LKMwafGSZ1tqdc1tGSa_ZBTeOU/edit?usp=sharing',
+        icon: 'tv',
+        text: '實驗步驟'
+      },
+      {
+        url: 'https://www.facebook.com/ETBlue/media_set?set=a.10210724638284792.1073741872.1014354995&type=3',
+        icon: 'photo',
+        text: '過程紀錄'
+      }
+    ]
+
+    this._staticData.topicObject = {}
+    topic.forEach((item, index) => {
+      this._staticData.topicObject[item.id] = item
+    })
+
+    this._staticData.teamObject = {}
+    team.forEach((item, index) => {
+      this._staticData.teamObject[item.id] = item
+    })
+
+    this._staticData.agendaObject = {}
+    agenda.forEach((item, index) => {
+      this._staticData.agendaObject[item.id] = item
+    })
+
+    this._staticData.meetingObject = {}
+    meeting.forEach((item, index) => {
+      this._staticData.meetingObject[item.id] = item
+    })
+
+    this._staticData.menuCount = {
+      'all': 0,
+      '1': 0,
+      '2': 0,
+      '3': 0,
+      '4': 0,
+      '5': 0
+    }
+
+    this._staticData.statusCount = {}
+    Object.keys(this._staticData.menuCount).forEach((key) => {
+      this._staticData.statusCount[key] = {
+        'all': {
+          name: '所有狀態',
+          count: 0,
+          color: 'black',
+          proposalIDs: []
+        },
+        'revoked': {
+          name: '已撤案',
+          count: 0,
+          color: 'red',
+          proposalIDs: []
+        },
+        'others': {
+          name: '放置 play',
+          count: 0,
+          color: '',
+          proposalIDs: []
+        },
+        'failed': {
+          name: '不通過',
+          count: 0,
+          color: 'red',
+          proposalIDs: []
+        },
+        'passed': {
+          name: '通過',
+          count: 0,
+          color: 'green',
+          proposalIDs: []
+        }
+      }
+    })
+
+    this._staticData.proposalData = proposal.map((item, index) => {
+
+      item.teamID = item._team.toString()
+
+      item.statusID = ''
+      if (item.voting === 'passed' || item.voting === 'failed') {
+        item.statusID = item.voting
+      } else if (item.proposal === 'revoked') {
+        item.statusID = item.proposal
+      } else {
+        item.statusID = 'others'
+      }
+
+      item.statusColor = ''
+      if (item.statusID === 'revoked' || item.statusID === 'failed') {
+        item.statusColor = 'red'
+      } else if (item.statusID === 'passed') {
+        item.statusColor = 'green'
+      }
+
+      if ( item.teamID.length > 0 && this._staticData.teamObject[item.teamID] ) {
+        this._staticData.menuCount[item.teamID] += 1
+      }
+      this._staticData.menuCount.all += 1
+
+      this._staticData.statusCount[item.teamID][item.statusID].proposalIDs.push(index)
+      this._staticData.statusCount[item.teamID].all.proposalIDs.push(index)
+      this._staticData.statusCount.all[item.statusID].proposalIDs.push(index)
+      this._staticData.statusCount.all.all.proposalIDs.push(index)
+
+      item.processClassNames = {
+        proposal: item.proposal === 'passed' ? 'completed' : ( item.proposal === 'revoked' ? 'failed' : 'disabled' ) ,
+        discussion: item.discussion === 'done' ? 'completed' : ( item.proposal === 'null' ? 'failed' : 'disabled' ) ,
+        vote4voting: item.vote4voting === 'passed' ? 'completed' : ( item.vote4voting === 'failed' ? 'failed' : 'disabled' ) ,
+        voting: item.voting === 'passed' ? 'completed' : ( item.voting === 'failed' ? 'failed' : 'disabled' )
+      }
+
+      item.processDescriptions = {
+        proposal: item.proposal === 'passed' ? '已通過' : ( item.proposal === 'revoked' ? '已撤銷' : '狀態不明' ) ,
+        discussion: item.discussion === 'done' ? '已討論' : ( item.proposal === 'null' ? '未討論' : ( item.proposal === 'n/a' ? '不適用' : '狀態不明') ) ,
+        vote4voting: item.vote4voting === 'passed' ? '已通過' : ( item.vote4voting === 'failed' ? '已否決' : '不適用' ) ,
+        voting: item.voting === 'passed' ? '已通過' : ( item.voting === 'failed' ? '已否決' : '狀態不明' )
+      }
+
+      return item
+
+    })
+
 
   }
 
@@ -49,86 +198,64 @@ class App extends Component {
 
   render() {
 
-    let topicObject = {}
+    const {
+      about,
+      topicObject, 
+      teamObject, 
+      agendaObject, 
+      meetingObject,
+      menuCount,
+      statusCount,
+      proposalData
+    } = this._staticData
 
-    topic.forEach((item, index) => {
-      topicObject[item.id] = item
+    const aboutJSX = about.map((item, index) => {
+      return (
+        <a key={index} className='item' target='_blank' href={item.url}>
+          <i className={`icon ${item.icon}`} />
+          <span className='content'>
+          {item.text}
+          </span>
+        </a>
+      )
     })
 
-    let teamObject = {}
-
-    team.forEach((item, index) => {
-      teamObject[item.id] = item
+    const menuJSX = Object.keys(menuCount).map((itemID, index) => {
+      return (
+        <div className={`item ${this.state.teamfilter === itemID ? 'active' : ''}`} key={index} data-teamfilter={ itemID } style={{cursor: 'pointer'}} onClick={this._filter}>
+          { teamObject[itemID] ? teamObject[itemID].name : '所有組別' }
+          <span className={`ui horizontal label ${this.state.teamfilter === itemID ? 'black' : ''}`} data-teamfilter={ itemID }>
+            { menuCount[itemID] }
+          </span>
+        </div>
+      )
     })
 
-    let agendaObject = {}
-
-    agenda.forEach((item, index) => {
-      agendaObject[item.id] = item
+    const submenuJSX = Object.keys(statusCount[this.state.teamfilter]).map((key) => {
+      return (
+        <div className={`item ${this.state.statusfilter === key ? 'active' : ''}`} key={key} data-statusfilter={ key } style={{cursor: 'pointer'}} onClick={this._filter}>
+          { statusCount[this.state.teamfilter][key].name }
+          <span className={`ui horizontal ${statusCount[this.state.teamfilter][key].color} label`} data-statusfilter={ key }>
+            { statusCount[this.state.teamfilter][key].proposalIDs.length }
+          </span>
+        </div>
+      )
     })
 
-    let meetingObject = {}
+    const proposalJSX = statusCount[this.state.teamfilter][this.state.statusfilter].proposalIDs.map((proposalID, index) => {
 
-    meeting.forEach((item, index) => {
-      meetingObject[item.id] = item
-    })
+      const item = proposalData[proposalID]
 
-    let menuCount = {
-      'all': 0,
-      '1': 0,
-      '2': 0,
-      '3': 0,
-      '4': 0,
-      '5': 0
-    }
-
-    let statusCount = {
-      'all': {
-        name: '所有狀態',
-        count: 0,
-        color: 'black'
-      },
-      'revoked': {
-        name: '已撤案',
-        count: 0,
-        color: 'red'
-      },
-      'others': {
-        name: '放置 play',
-        count: 0,
-        color: ''
-      },
-      'failed': {
-        name: '不通過',
-        count: 0,
-        color: 'red'
-      },
-      'passed': {
-        name: '通過',
-        count: 0,
-        color: 'green'
-      }
-    }
-
-    const proposalJSX = proposal.map((item, index) => {
-
-      let teamID = item._team.toString()
-
-      let statusID
-      if (item.voting === 'passed' || item.voting === 'failed') {
-        statusID = item.voting
-      } else if (item.proposal === 'revoked') {
-        statusID = item.proposal
-      } else {
-        statusID = 'others'
-      }
+      const {
+        teamID, 
+        statusID, 
+        processClassNames, 
+        processDescriptions,
+        statusColor
+      } = item
 
       let teamJSX
       if ( teamID.length > 0 && teamObject[teamID] ) {
-
-        menuCount[teamID] += 1
-        menuCount.all += 1
-
         teamJSX = (
           <a className='ui horizontal teal label' style={{textAlign: 'left'}} href={teamObject[teamID].url} target='_blank'>
             <i className='icon linkify' />
@@ -136,33 +263,6 @@ class App extends Component {
           </a>
         )
       } 
-
-      if ( (this.state.statusfilter !== 'all' && this.state.statusfilter !== statusID) || ( this.state.teamfilter !== 'all' && this.state.teamfilter !== teamID ) ) {
-
-        if (this.state.teamfilter === teamID || this.state.teamfilter === 'all') {
-          statusCount[statusID].count += 1
-          statusCount.all.count += 1
-        }
-
-        return null
-      }
-
-      statusCount[statusID].count += 1
-      statusCount.all.count += 1
-
-      const classNames = {
-        proposal: item.proposal === 'passed' ? 'completed' : ( item.proposal === 'revoked' ? 'failed' : 'disabled' ) ,
-        discussion: item.discussion === 'done' ? 'completed' : ( item.proposal === 'null' ? 'failed' : 'disabled' ) ,
-        vote4voting: item.vote4voting === 'passed' ? 'completed' : ( item.vote4voting === 'failed' ? 'failed' : 'disabled' ) ,
-        voting: item.voting === 'passed' ? 'completed' : ( item.voting === 'failed' ? 'failed' : 'disabled' )
-      }
-
-      const description = {
-        proposal: item.proposal === 'passed' ? '已通過' : ( item.proposal === 'revoked' ? '已撤銷' : '狀態不明' ) ,
-        discussion: item.discussion === 'done' ? '已討論' : ( item.proposal === 'null' ? '未討論' : ( item.proposal === 'n/a' ? '不適用' : '狀態不明') ) ,
-        vote4voting: item.vote4voting === 'passed' ? '已通過' : ( item.vote4voting === 'failed' ? '已否決' : '不適用' ) ,
-        voting: item.voting === 'passed' ? '已通過' : ( item.voting === 'failed' ? '已否決' : '狀態不明' )
-      }
 
       const topicJSX = item._topic.split('、').map((t, i) => {
         if (t.length > 0 && topicObject[t]) {
@@ -201,13 +301,6 @@ class App extends Component {
         })
       }
 
-      let statusColor
-      if (statusID === 'revoked' || statusID === 'failed') {
-        statusColor = 'red'
-      } else if (statusID === 'passed') {
-        statusColor = 'green'
-      }
-
       return (
         <div className='ui segments' key={ index }>
           <div className={`ui ${statusColor} segment`}>
@@ -217,47 +310,47 @@ class App extends Component {
           </div>
           <div className={`ui left aligned segment`} style={{backgroundColor: 'rgba(0,0,0,0.05)'}}>
             <div className='ui fluid steps'>
-              <div className={`step ${classNames.proposal}`}>
+              <div className={`step ${processClassNames.proposal}`}>
                 <i className='icon text file outline' />
                 <div className='content'>
                   <div className='title'>
                     提案
                   </div>
                   <div className='description'>
-                    { description.proposal }
+                    { processDescriptions.proposal }
                   </div>
                 </div>
               </div>
-              <div className={`step ${classNames.discussion}`}>
+              <div className={`step ${processClassNames.discussion}`}>
                 <i className='icon comments outline' />
                 <div className='content'>
                   <div className='title'>
                     討論
                   </div>
                   <div className='description'>
-                    { description.discussion }
+                    { processDescriptions.discussion }
                   </div>
                 </div>
               </div>
-              <div className={`step ${classNames.vote4voting}`}>
+              <div className={`step ${processClassNames.vote4voting}`}>
                 <i className='icon hand rock' />
                 <div className='content'>
                   <div className='title'>
                     程序表決
                   </div>
                   <div className='description'>
-                    { description.vote4voting }
+                    { processDescriptions.vote4voting }
                   </div>
                 </div>
               </div>
-              <div className={`step ${classNames.voting}`}>
+              <div className={`step ${processClassNames.voting}`}>
                 <i className='icon hand paper' />
                 <div className='content'>
                   <div className='title'>
                     內容表決
                   </div>
                   <div className='description'>
-                    { description.voting }
+                    { processDescriptions.voting }
                   </div>
                 </div>
               </div>
@@ -282,31 +375,6 @@ class App extends Component {
       )
     })
 
-    const menuJSX = team.map((item, index) => {
-
-      const itemID = item.id.toString()
-
-      return (
-        <div className={`item ${this.state.teamfilter === itemID ? 'active' : ''}`} key={index} data-teamfilter={ itemID } style={{cursor: 'pointer'}} onClick={this._filter}>
-          { item.name }
-          <span className='ui horizontal label' data-teamfilter={ itemID }>
-            { menuCount[itemID] }
-          </span>
-        </div>
-      )
-    })
-
-    const submenuJSX = Object.keys(statusCount).map((key) => {
-      return (
-        <div className={`item ${this.state.statusfilter === key ? 'active' : ''}`} key={key} data-statusfilter={ key } style={{cursor: 'pointer'}} onClick={this._filter}>
-          { statusCount[key].name }
-          <span className={`ui horizontal ${statusCount[key].color} label`} data-statusfilter={ key }>
-            { statusCount[key].count }
-          </span>
-        </div>
-      )
-    })
-
     return (
       <div className='App'>
         <div className='App-header ui basic inverted segment'>
@@ -321,48 +389,13 @@ class App extends Component {
               一個 open data 結合公民審議的小實驗，以 2017 總統府司法改革國是會議為例，從上游的資料內容產生、到下游的衍生應用開發，藉由走過整個生產鏈，探索 open data 從理論到實做的過程中可能的瓶頸。本網頁《司改國是 2017 - 提案闖天關》是下游衍生應用的範例，未來的公民審議如果有從上游做結構化資料，那麼除了類似本頁的議題履歷之外，還可以發展出更多不同類型的有趣的衍生應用。詳細的實驗心得會另外寫在部落格（還沒寫 XD）
             </p>
             <div className='ui inverted horizontal link list'>
-              <a className='item' target='_blank' href='https://etblue.github.io/jrpilot/'>
-                <i className='icon globe' />
-                <span className='content'>
-                範例
-                </span>
-              </a>
-              <a className='item' target='_blank' href='https://github.com/ETBlue/jrpilot'>
-                <i className='icon github' />
-                <span className='content'>
-                程式碼
-                </span>
-              </a>
-              <a className='item' target='_blank' href='https://docs.google.com/spreadsheets/d/1CTGZfJ55WJQEq6UexllrXZJvceVP3TRjzNGMgxE-I68/edit?usp=sharing'>
-                <i className='icon table' />
-                <span className='content'>
-                資料
-                </span>
-              </a>
-              <a className='item' target='_blank' href='https://docs.google.com/presentation/d/18QjhH784zyxAxGue7LKMwafGSZ1tqdc1tGSa_ZBTeOU/edit?usp=sharing'>
-                <i className='icon tv' />
-                <span className='content'>
-                實驗步驟
-                </span>
-              </a>
-              <a className='item' target='_blank' href='https://www.facebook.com/ETBlue/media_set?set=a.10210724638284792.1073741872.1014354995&type=3'>
-                <i className='icon photo' />
-                <span className='content'>
-                過程紀錄
-                </span>
-              </a>
+              { aboutJSX }
             </div>
             <hr className='ui hidden divider' />
           </div>
         </div>
         <div className='ui container'>
           <div className='ui six item secondary pointing menu'>
-            <div className={`item ${this.state.teamfilter === 'all' ? 'active' : ''}`} data-teamfilter='all' style={{cursor: 'pointer'}} onClick={this._filter}>
-              全部
-              <span className='ui horizontal black label' data-teamfilter='all'>
-                { menuCount.all }
-              </span>
-            </div>
             { menuJSX }
           </div>
           <div className='ui five item secondary menu'>
